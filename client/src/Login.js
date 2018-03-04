bd521c8e984bf6d66693c26d8a3e0b65429f1a21
Nicolas Fetter <nicofetter@gmail.com>
2018-03-04 15:36:34 +1300

    redirecting when loging in

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import ImmutablerPropTypes from 'react-immutable-proptypes'
 
+import * as authSelectors from './redux/auth/selectors'
 import * as authActions from './redux/auth/actions'
 
 class Login extends React.Component {
@@ -26,6 +29,10 @@ class Login extends React.Component {
 
   render() {
     const { email, password } = this.state
+    const { currentUser } = this.props
+    if (currentUser) {
+      return <Redirect to="/" />
+    }
     return (<form onSubmit={this.handleSubmit}>
       Email <input type="text" name="email" value={email} onChange={this.handleChange} />
       Password <input type="text" name="password" value={password} onChange={this.handleChange} />
@@ -34,8 +41,16 @@ class Login extends React.Component {
   }
 }
 
+Login.propTypes = {
+  currentUser: ImmutablerPropTypes.map,
+}
+
+const mapStateToProps = state => ({
+  currentUser: authSelectors.getCurrentUser(state),
+})
+
 const mapDispatchToProps = dispatch => ({
   dispatchLogin: data => dispatch(authActions.login(data)),
 })
 
-export default connect(null, mapDispatchToProps)(Login)
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
