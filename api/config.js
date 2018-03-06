c1492e4471b7ac17d7099aa3705c9c94d8d1cb8f
Nicolas Fetter <nicofetter@gmail.com>
2018-03-06 20:12:02 +1300

    adding mongodb url

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_NAME = 'example_db'
-const MONGO_URL = process.env.MONGO_DB_URL || `mongodb://localhost:27017/${MONGO_DB_NAME}`
+const MONGO_URL = process.env.MONGODB_URI || `mongodb://localhost:27017/${MONGO_DB_NAME}`
 
 module.exports = {
   MONGO_URL,
