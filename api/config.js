37c75618f94d28dea1e8afe6993fde4afe136940
Nicolas Fetter <nicofetter@gmail.com>
2018-03-06 20:07:47 +1300

    adding env variable for mongodb url

--- api/config.js
+++ api/config.js
@@ -1,7 +1,6 @@
 const MONGO_DB_NAME = 'example_db'
-const MONGO_URL = `mongodb://localhost:27017/${MONGO_DB_NAME}`
+const MONGO_URL = process.env.MONGO_DB_URL || `mongodb://localhost:27017/${MONGO_DB_NAME}`
 
 module.exports = {
   MONGO_URL,
-  MONGO_DB_NAME,
 }
