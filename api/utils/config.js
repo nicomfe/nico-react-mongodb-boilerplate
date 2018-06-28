fa3bf55f1f08e1b8bd26b0e00e6bf39d9f83a930
Nicolas Fetter <nicofetter@gmail.com>
2018-06-28 12:25:29 +1200

    Adding timestamp to logs

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,9 +1,10 @@
 const MONGO_DB_NAME = 'example_db'
 
-const { EMAIL_HOST, EMAIL_USER, EMAIL_PASS, EMAIL_PORT, APP_HOST, MONGODB_URI } = process.env
+const { EMAIL_HOST, EMAIL_USER, EMAIL_PASS, EMAIL_PORT, APP_HOST, MONGODB_URI, NODE_ENV } = process.env
 
 const MONGO_URL = MONGODB_URI || `mongodb://localhost:27017/${MONGO_DB_NAME}`
 const APP_HOST_URL = APP_HOST || 'http://localhost:3000'
+const LOG_LEVEL_BY_ENV = NODE_ENV === 'production' ? 'error' : 'info'
 
 module.exports = {
   MONGO_DB_NAME,
@@ -13,5 +14,5 @@ module.exports = {
   EMAIL_PASS,
   EMAIL_PORT,
   APP_HOST_URL,
-  LOG_LEVEL: process.env.LOG_LEVEL || 'error',
+  LOG_LEVEL: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : LOG_LEVEL_BY_ENV,
 }
--- api/utils/logger.js
+++ api/utils/logger.js
@@ -1,30 +1,33 @@
-const winston = require('winston')
+const { createLogger, format, transports } = require('winston')
 
 const config = require('./config')
 
-winston.level = config.LOG_LEVEL
+const myFormat = format.printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`
+})
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+const logger = createLogger({
+  level: config.LOG_LEVEL,
+  format: format.combine(
+    format.timestamp(),
+    myFormat
+  ),
   transports: [
     //
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
   ],
 })
 
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }))
+  logger.add(new transports.Console({ colorize: true }))
 }
 
-exports.error = message => logger.log('error', message)
-exports.warn = message => logger.log('warn', message)
-exports.info = message => logger.log('info', message)
-exports.verbose = message => logger.log('verbose', message)
-exports.debug = message => logger.log('debug', message)
+exports.error = message => logger.log({ level: 'error', message })
+exports.warn = message => logger.log({ level: 'warn', message })
+exports.info = message => logger.log({ level: 'info', message })
+exports.verbose = message => logger.log({ level: 'verbose', message })
+exports.debug = message => logger.log({ level: 'debug', message })
