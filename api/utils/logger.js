e6f3a616b759ae1ff331dbd8dc131ddfeddf8cf8
Nicolas Fetter <nicofetter@gmail.com>
2018-06-28 14:17:53 +1200

    removing file log cause its not created in heroku

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -2,27 +2,13 @@ const { createLogger, format, transports } = require('winston')
 
 const config = require('./config')
 
-const myFormat = format.printf(info => `${info.level === 'error' ? info.timestamp : ''} ${info.level}: ${info.message}`)
+const myFormat = format.printf(info => `${info.level}: ${info.message}`)
 
 const logger = createLogger({
   level: config.LOG_LEVEL,
-  format: format.combine(
-    format.timestamp(),
-    myFormat
-  ),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new transports.Console({ colorize: true }),
-  ],
+  format: myFormat,
+  transports: [new transports.Console({ colorize: true })],
 })
-
-if (process.env.NODE_ENV === 'production') {
-  // in prod I log errors to the file error.log
-  logger.add(new transports.File({ filename: 'error.log', level: 'error' }))
-}
 
 exports.error = message => logger.log({ level: 'error', message })
 exports.warn = message => logger.log({ level: 'warn', message })
